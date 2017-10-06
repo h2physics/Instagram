f4bd62a6e6255d7570f9d38b0da367d2df059eec
h2physics <nhoxlazyhamchoi@gmail.com>
2017-10-07 02:04:52 +0700

    create PhotoAdapter

---
+++ app/src/main/java/com/h2physics/instagram/ui/adapter/PhotoAdapter.java
@@ -0,0 +1,40 @@
+package com.h2physics.instagram.ui.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.h2physics.instagram.data.model.Photo;
+import com.h2physics.instagram.ui.base.BaseAdapter;
+import com.h2physics.instagram.ui.base.BaseViewHolder;
+
+/**
+ * Created by YukiNoHara on 10/7/2017.
+ */
+
+public class PhotoAdapter extends BaseAdapter<Photo> {
+    @Override
+    public int getContentView() {
+        return 0;
+    }
+
+    @Override
+    public BaseViewHolder<Photo> onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(BaseViewHolder<Photo> holder, int position) {
+
+    }
+
+    public class PhotoViewHolder extends BaseViewHolder<Photo>{
+        public PhotoViewHolder(View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        public void bind(Photo photo, int position) {
+
+        }
+    }
+}
