a12f5c879cac5428468f92153bcc478be6474ca6
h2physics <nhoxlazyhamchoi@gmail.com>
2017-10-07 01:59:17 +0700

    create TestActivity

---
+++ app/src/main/java/com/h2physics/instagram/ui/example/TestActivity.java
@@ -0,0 +1,15 @@
+package com.h2physics.instagram.ui.example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.h2physics.instagram.R;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
